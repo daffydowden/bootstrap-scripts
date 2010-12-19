82eec982c26ec765de7019ff5fcc2e14aaf64e3a
Richard Dowden <richard.dowden@purpletonic.com>
2010-12-19 18:39:15 +0000

    save to network interfaces file

--- rackspace/01-iptables.sh
+++ rackspace/01-iptables.sh
@@ -7,5 +7,4 @@
 iptables-restore < ../config/etc/iptables.up.rules
 iptables-save > /etc/iptables.up.rules
 
-perl -pi.bak -e "s/iface lo inet loopback/iface lo inet loopback\npre-up iptables-restore < \/etc\/iptables.up.rules/gi;" 
-                /etc/network/interfaces
+perl -pi.bak -e "s/iface lo inet loopback/iface lo inet loopback\npre-up iptables-restore < \/etc\/iptables.up.rules/gi;" /etc/network/interfaces
