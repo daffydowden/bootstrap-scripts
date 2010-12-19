1e485cfed162769daddf634eb08ec0e5646bfb31
Richard Dowden <richard.dowden@purpletonic.com>
2010-12-19 18:24:03 +0000

    changed iptables default location

--- rackspace/01-iptables.sh
+++ rackspace/01-iptables.sh
@@ -1,10 +1,10 @@
 #
-# Ubuntu 8.0.4
+# Ubuntu 10.04
 #
 # Update iptables
 #
 
-iptables-restore < ~/bootstrap/config/etc/iptables.up.rules
+iptables-restore < ../config/etc/iptables.up.rules
 iptables-save > /etc/iptables.up.rules
 
 perl -pi.bak -e "s/iface lo inet loopback/iface lo inet loopback\npre-up iptables-restore < \/etc\/iptables.up.rules/gi;" 
